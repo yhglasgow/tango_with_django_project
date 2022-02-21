1dd0592810f5f6fbe995c4866484e42d1bd88353
“Yuhao <2661960h@mstudent.gla.ac.uk>
2022-02-21 23:37:55 +0000

    Chapter6 Exercise

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
 from rango.models import Category
 from rango.models import Page
 
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -24,9 +24,10 @@ def show_category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
+        
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-    return render(request, 'rango/category.html', context=context_dict)
+    return render(request, 'rango/category.html', context_dict)
